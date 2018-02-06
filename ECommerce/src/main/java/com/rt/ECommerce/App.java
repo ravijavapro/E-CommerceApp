5a0092eb58fea3799929cb420767f14ec8452131
Ravi Tripathi <SONY@SONY-PC>
2018-02-07 01:45:05 +0530

    Project Added

---
+++ ECommerce/src/main/java/com/rt/ECommerce/App.java
@@ -0,0 +1,13 @@
+package com.rt.ECommerce;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
